4ecd7307e569a2422eb9fec16f428c35716b589c
Ade Yudys Triawan <yudistriawan@gmail.com>
2018-12-06 05:44:12 +0700

    added weather by last known location

--- app/src/main/java/com/example/yudystriawan/foodapp/MainActivity.java
+++ app/src/main/java/com/example/yudystriawan/foodapp/MainActivity.java
@@ -1,5 +1,10 @@
 package com.example.yudystriawan.foodapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -9,30 +14,54 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String WEATHER_LINK = "https://api.openweathermap.org/data/2.5/weather?q=Malang,id&appid=28c444227fbea12e1d303822b43f327f";
-    private TextView tLat, tLon, tTemp, tName, tDescription;
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private TextView tLat, tLon, tTemp, tName, tDescription, tMyLat, tMyLon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
         tLat = findViewById(R.id.lat);
         tLon = findViewById(R.id.lon);
         tTemp = findViewById(R.id.temp);
         tName = findViewById(R.id.name);
         tDescription = findViewById(R.id.description);
+        tMyLat = findViewById(R.id.myLat);
+        tMyLon = findViewById(R.id.myLon);
 
-        getWeather();
+        getLastKnownLocation();
     }
 
-    private void getWeather() {
+    private void getLastKnownLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},REQUEST_LOCATION_PERMISSION );
+        }
+        mFusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    getWeather(location.getLatitude(), location.getLongitude());
+                }
+            }
+
+        });
+    }
+
+    private void getWeather(double latitude, double longitude) {
+        String WEATHER_LINK = "https://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&appid=28c444227fbea12e1d303822b43f327f";
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, WEATHER_LINK, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
